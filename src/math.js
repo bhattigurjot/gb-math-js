75d7abe16db8112885caae9fb534f309f0e42491
bhattigurjot <bhattigurjot@gmail.com>
2019-10-29 00:40:55 -0600

    Created basic class and added some settings for babel

--- src/math.js
+++ src/math.js
@@ -2,4 +2,15 @@ const add = (a,b) => {
     return a+b
 }
 
-export { add as default }
+class vec2d {
+    constructor(_x=0, _y=0) {
+        this.x = _x;
+        this.y = _y;
+    }
+
+    setX(_x) {
+        this.x = _x;
+    }
+}
+
+export { vec2d, add as default }
--- tests/math.test.js
+++ tests/math.test.js
@@ -1,5 +1,13 @@
-import add from '../src/math'
+import add, {vec2d} from '../src/math'
 
-test('demo test', () => {
+test('demo function test', () => {
     expect(add(1,1)).toBe(2)
+})
+
+test('demo class test', () => {
+    const v = new vec2d()
+    expect(v.x).toBe(0)
+    expect(v.y).toBe(0)
+    v.setX(5)
+    expect(v.x).toBe(5)
 })
