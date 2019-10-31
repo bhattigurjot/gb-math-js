7d8a74697a4c2527940ddfa2650a2e79fd2e301e
sanjeetbhatti <edu.sanjeet@gmail.com>
2019-10-30 20:52:02 -0600

    added cross product function with its test case
    also renamed variables in tests.

--- src/math.js
+++ src/math.js
@@ -44,6 +44,12 @@ class vec2d {
         let s = (_v1.x * _v2.x) + (_v1.y * _v2.y)
         return s
     }
+
+    //cross product on 2-D vectors results in a scalar value, not a vector product.
+    static crossProduct(_v1, _v2) {
+        let s = (_v1.x * _v2.y) - (_v1.y * _v2.x)
+        return s
+    }
 }
 
 export { vec2d }
--- tests/math.test.js
+++ tests/math.test.js
@@ -23,53 +23,58 @@ test('Should be able to change vector components', () => {
 })
 
 test('Should return correct addition resultant vector', () => {
-    let v_r = v1.add(v2)
-    expect(v_r.x).toBe(5)
-    expect(v_r.y).toBe(9)
-    expect(v_r).toMatchObject({x: 5, y: 9})
+    let v_add = v1.add(v2)
+    expect(v_add.x).toBe(5)
+    expect(v_add.y).toBe(9)
+    expect(v_add).toMatchObject({x: 5, y: 9})
 
-    let v3 = vec2d.addVectors(v1, v2)
-    expect(v3).toMatchObject({x: 5, y: 9})
+    let v_add_2 = vec2d.addVectors(v1, v2)
+    expect(v_add_2).toMatchObject({x: 5, y: 9})
 })
 
 test('Should return correct subtraction resultant vector', () => {
-    let v3 = vec2d.subVectors(v1, v2)
-    expect(v3).toMatchObject({x: -1, y: 1})
+    let v_sub = vec2d.subVectors(v1, v2)
+    expect(v_sub).toMatchObject({x: -1, y: 1})
 })
 
 test('Should return correct multiplication resultant vector (Vector by a Scalar)', () => {
-    let v3 = vec2d.mulScalar(v1, 5)
-    expect(v3).toMatchObject({x: 10, y: 25})
+    let v_mul_1 = vec2d.mulScalar(v1, 5)
+    expect(v_mul_1).toMatchObject({x: 10, y: 25})
 
-    let v4 = vec2d.mulScalar(v1, -6)
-    expect(v4).toMatchObject({x: -12, y: -30})
+    let v_mul_2 = vec2d.mulScalar(v1, -6)
+    expect(v_mul_2).toMatchObject({x: -12, y: -30})
 })
 
 test('Should return correct division resultant vector (Vector by a Scalar)', () => {
-    let v3 = vec2d.divScalar(v1, 5)
-    expect(v3).toMatchObject({x: 0.4, y: 1})
+    let v_div_1 = vec2d.divScalar(v1, 5)
+    expect(v_div_1).toMatchObject({x: 0.4, y: 1})
 
     //another edge case - when one result is without decimal digits i.e. a whole number.
     //this gives an error - cannot read property length for 'y'
 
-    let v4 = vec2d.divScalar(v1, -4)
-    expect(v4).toMatchObject({x: -0.5, y: -1.25})
+    let v_div_2 = vec2d.divScalar(v1, -4)
+    expect(v_div_2).toMatchObject({x: -0.5, y: -1.25})
 
     //edge case - when result is non-terminating - expecting 5 decimal places
-    let v5 = vec2d.divScalar(v1, -6)
-    expect(v5).toMatchObject({x: -0.33333, y: -0.83333})
+    let v_div_3 = vec2d.divScalar(v1, -6)
+    expect(v_div_3).toMatchObject({x: -0.33333, y: -0.83333})
 
     //there will not be any case where one value(either x or y) will be terminating
     //and the other non-terminating because of being divided by the same denominator.
 
     //edge case - divide by zero
-    let v6 = () => {
+    let v_div_4 = () => {
         vec2d.divScalar(v1, 0)
     }
-    expect(v6).toThrow("Cannot divide by zero")
+    expect(v_div_4).toThrow("Cannot divide by zero")
 })
 
 test('Should return the Dot Product of two vectors', () => {
-    let v7 = vec2d.dotProduct(v1, v2)
-    expect(v7).toBe(26)
+    let v_dot = vec2d.dotProduct(v1, v2)
+    expect(v_dot).toBe(26)
+})
+
+test('Should return the Cross Product of two vectors', () => {
+    let v_cross = vec2d.crossProduct(v1, v2)
+    expect(v_cross).toBe(-7)
 })
